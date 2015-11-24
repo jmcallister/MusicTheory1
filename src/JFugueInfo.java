d50d45a9b770f4871c955cf4fd78e66e948faed2
Joe McAllister <jmcallister@micds.org>
2015-11-24 16:24:39 -0600

    Added 3 examples for you Ben

--- src/JFugueInfo.java
+++ src/JFugueInfo.java
@@ -6,17 +6,22 @@
  * Import statement: import org.jfugue.*; 
  * IGNORE WARNING; Eclipse issue that does not cause any errors
  * Note: jFugue 5.0 is available but it hasn't been used before and there is very limited documentation
+ * 
+ * uncomment any player.play lines if you want to hear them
  */
 import org.jfugue.*; 
 public class JFugueInfo 
 {
 	public static void main(String[] args)
 	{
-		Player player1 = new Player(); //sounds like shit but something to look at
-		player1.play("Cq Dbq Eq Fq | Ch Dbq Eq | F#i Di Gh."); //period equals dotted note
+		Player player = new Player(); 
 		
-		Player player2 = new Player();
+		player.play("Cq Dbq Eq Fq | Ch Dbq Eq | F#i Di Gh."); //period:dotted_note #:sharp b:flat
+	    
+		//player.play("E5s A5s C6s B5s E5s B5s D6s C6i E6i G#5i E6i | A5s E5s A5s C6s B5s E5s B5s D6s C6i A5i Ri");//this sounds better
 		
+		//player.play("V0 C4w | G4w | C4w " + "V1 Ew | Dw | Dq. Eh.. " + "V2 Gw | Bw | Gw"); //multiple parts...V1 has a suspension!
+		//number after note specifies octave (range 1-9; C4 is middle C
 	}
 
 }
