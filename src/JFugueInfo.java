c8d84f5c96771acde48e7a0e434d32c3cfe79ea6
Joe McAllister <jmcallister@micds.org>
2015-11-24 15:44:28 -0600

    This creates a class with download information and basic examples of jFugue

---
+++ src/JFugueInfo.java
@@ -0,0 +1,11 @@
+/*
+ * Download jFugue (jfugue-4.0.3.jar): http://www.jfugue.org/4/download.html
+ * All we need to know: http://www.jfugue.org/4/book.html
+ * Basic Examples: http://www.jfugue.org/4/examples.html
+ * 
+ * Note: jFugue 5.0 is available but it hasn't been used before and there is very limited documentation
+ */
+public class JFugueInfo 
+{
+
+}
